d97a3c97d339e3ab544e2547da28ee080f4eb62a
seungheondoh <seungheondoh@kaist.ac.kr>
2023-08-10 06:27:23 +0000

    update

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -5,87 +5,84 @@ import pandas as pd
 import numpy as np
 import multiprocessing
 from functools import partial
-from contextlib import contextmanager
 from audio_utils import load_audio
 from io_utils import _json_dump
 from constants import DATASET, DATA_LENGTH, STR_CH_FIRST, MUSIC_SAMPLE_RATE, KEY_DICT
 
-@contextmanager
-def poolcontext(*args, **kwargs):
-    pool = multiprocessing.Pool(*args, **kwargs)
-    yield pool
-    pool.terminate()
-    
 def gtzan_resampler(genre, path):
     src, _ = load_audio(
         path=os.path.join(DATASET,'gtzan','audio', genre, path),
         ch_format= STR_CH_FIRST,
         sample_rate= MUSIC_SAMPLE_RATE,
         downmix_to_mono= True)
-    if src.shape[-1] < DATA_LENGTH: # short case
-        pad = np.zeros(DATA_LENGTH)
-        pad[:src.shape[-1]] = src
-        src = pad
-    elif src.shape[-1] > DATA_LENGTH: # too long case
-        src = src[:DATA_LENGTH]
-    save_name = os.path.join(DATASET,'gtzan','npy', path.replace(".wav",".npy"))
-    if not os.path.exists(os.path.dirname(save_name)):
-        os.makedirs(os.path.dirname(save_name))
-    np.save(save_name, src.astype(np.float32))
-    
-def get_track_split(gtzan_path):
-    tr_names = os.path.join(gtzan_path, "GTZAN_split/train_filtered.txt")
-    va_names = os.path.join(gtzan_path, "GTZAN_split/valid_filtered.txt")
-    te_names = os.path.join(gtzan_path, "GTZAN_split/test_filtered.txt")
-    track_split = {
-        "train_track": open(tr_names,'r').read().splitlines(),
-        "valid_track": open(va_names,'r').read().splitlines(),
-        "test_track": open(te_names,'r').read().splitlines()
-    }
-    with open(os.path.join(gtzan_path, "track_split.json"), mode="w") as io:
-        json.dump(track_split, io, indent=4)
-    track_list = track_split['train_track'] + track_split['valid_track'] + track_split['test_track']
-    track_list = [i.split("/")[1] for i in track_list]
-    return track_list
+    return src
 
-def get_quantize_tempo(gtzan_path, track_list):
-    df = pd.read_csv(os.path.join(gtzan_path, "GTZAN-Rhythm_v2_ismir2015_lbd/stats.csv"), index_col=0)
-    gtzan_filtered = df.loc[track_list]
-    gtzan_filtered = gtzan_filtered.rename(columns = {'artist' : 'artist_name'})
-    gtzan_filtered['tempo'] = gtzan_filtered['tempo mean'].astype(int)
-    gtzan_tempo = gtzan_filtered[["artist_name","title","tempo"]]
-    return gtzan_tempo
+def load_rhythms(fname):
+    samples = json.load(open(f"../dataset/rhythm/marchand2015swing/jams/{fname}.jams", 'r'))
+    sequences = {'beat':[], 'downbeat':[], '8th-note':[]}
+    for ann in samples['annotations']:
+        if "annotation_type" in ann['sandbox']:
+            _type = ann['sandbox']['annotation_type']
+            data = [i['time'] for i in ann['data']]
+            sequences[_type] = data
+        else:
+            sequences.update(ann['sandbox'])
+    return {"_".join(i.split()):j for i,j in sequences.items()}
 
-def get_key_info(gtzan_path, gtzan_tempo):
-    root = os.path.join(gtzan_path, "gtzan_key/gtzan_key/genres/")
-    tag_list, key_list = [], []
-    for i in gtzan_tempo.index:
-        genre = i.split(".")[0]
-        fname = i.replace('.wav','.lerch.txt')
-        value = open(os.path.join(root, genre, fname),'r').read()
-        key_list.append(KEY_DICT[value.strip()])
-        tag_list.append(genre)
-    gtzan_tempo['key'] = key_list
-    gtzan_tempo['tag'] = tag_list
-    return gtzan_tempo
+def load_metrical_annotation():
+    metrical_annotation = json.load(open("../dataset/rhythm/quinton2015extraction/annotations_MetricalStructure_GTZAN.json", 'r'))
+    metrical_levels_pulse_rates = {instance['trackName']: [i['metrical_levels_pulse_rates'] for i in instance['data']] for instance in metrical_annotation}
+    annotator = {instance['trackName']: [i['annotator'] for i in instance['data']] for instance in metrical_annotation}
+    return metrical_levels_pulse_rates, annotator
 
-def GTZAN_processor(gtzan_path):
-    track_list = get_track_split(gtzan_path)
-    gtzan_tempo = get_quantize_tempo(gtzan_path, track_list)
-    gtzan_tempo_key = get_key_info(gtzan_path, gtzan_tempo)
-    gtzan_final = gtzan_tempo_key[["artist_name","title","key","tempo","tag"]]
-    gtzan_final.index.name = "track_id"
-    gtzan_final['track_id'] = gtzan_final.index
-    gtzan_final = gtzan_final.fillna(0)
-    kv_dataset = gtzan_final.to_dict('index')
-    tag_stats = {i:j for i,j in Counter(gtzan_final['tag']).most_common()}
-    tag_info = {i:["genre"] for i in set(gtzan_final['tag'])}
-    _json_dump(os.path.join(gtzan_path, 'annotation.json'), kv_dataset)
-    _json_dump(os.path.join(gtzan_path, 'gtzan_tags.json'), list(set(gtzan_final['tag'])))
-    _json_dump(os.path.join(gtzan_path, 'gtzan_tag_stats.json'), tag_stats)
-    _json_dump(os.path.join(gtzan_path, 'gtzan_tag_info.json'), tag_info)
-    
-    with poolcontext(processes=multiprocessing.cpu_count()-5) as pool:
-        pool.starmap(gtzan_resampler, zip(list(gtzan_final['tag']), list(gtzan_final.index)))
+def load_caption():
+    df_cap = pd.read_csv("../dataset/caption/denk2023brain2music/brain2music-captions.csv").to_dict(orient="records")
+    id2caps = {i["ID"].replace("_15s", ""):i["DescEN"] for i in df_cap}
+    id2caps['disco.00011'] = id2caps['disco.000011'] # update wrong annotation
+    return id2caps
 
-    print("finish gtzan extract", len(track_list))
+def load_meta_json(meta_path, track_id):
+    return json.load(open(f"{meta_path}/{track_id}.json", "r"))
+
+def _parsing_meta(temp):
+    metadata, lyrics, youtube_url = {}, "", ""
+    for section in temp['track']['sections']:
+        if section['type'] == "SONG":
+            metadata = {i["title"].lower() :i["text"] for i in section['metadata']}
+        elif section['type'] == "LYRICS":
+            lyrics = section['text']
+        elif section['type'] == "VIDEO":
+            youtube_url = section['youtubeurl']
+            r = requests.get(youtube_url)
+            yt_url = r.json()['actions'][0]['uri'].replace("?autoplay=1","")
+    return metadata, lyrics, yt_url
+
+def load_strum_split(sturm_path):
+    tr_names = os.path.join(sturm_path, "train_filtered.txt")
+    va_names = os.path.join(sturm_path, "valid_filtered.txt")
+    te_names = os.path.join(sturm_path, "test_filtered.txt")
+        
+    train_track = set(open(tr_names,'r').read().splitlines())
+    valid_track = set(open(va_names,'r').read().splitlines())
+    test_track = set(open(te_names,'r').read().splitlines())
+    return train_track, valid_track, test_track
+
+def load_key_info(key_path, path):
+    key_value = open(f"{key_path}/{path}".replace(".wav",".lerch.txt"),'r').read()
+    key_info = KEY_DICT[key_value.strip()]
+    return key_info
+
+def load_strum_metadata():
+    strum_fingerprint = {}
+    for i in pd.read_csv("../dataset/rhythm/marchand2015swing/stats.csv").to_dict(orient = "records"):
+        _id = i["filename"].replace(".wav", "")
+        if type(i['artist']) == str:
+            artist = i['artist']
+        else:
+            artist = ""
+        strum_fingerprint[_id] = {
+            "id": _id,
+            "artist": artist,
+            "title": i['title']
+        }
+    return strum_fingerprint
